d2be6a43fd1a1dfa2ea86862c7b721a277231cdf
Benjamin Anderson <andersonbcdefg@gmail.com>
2024-05-24 20:38:22 -0700

    extend position embs function

---
+++ src/bertgery/operators/extend_positions.py
@@ -0,0 +1,21 @@
+from transformers import BertModel
+import torch.nn as nn
+
+def extend_positions(model: BertModel, max_position: int):
+    old_position_embeddings = model.embeddings.position_embeddings
+    new_position_embeddings = nn.Embedding(
+        max_position, 
+        old_position_embeddings.weight.size(1)
+    )
+    avg_old_embedding = old_position_embeddings.weight.mean(dim=0)
+
+    # copy over old positions
+    new_position_embeddings.weight.data[:old_position_embeddings.weight.size(0)] = \
+        old_position_embeddings.weight.data
+
+    # initialize new positions to the average
+    new_position_embeddings.weight.data[old_position_embeddings.weight.size(0):] = \
+        avg_old_embedding
+    
+    model.embeddings.position_embeddings = new_position_embeddings
+    model.config.max_position_embeddings = max_position
